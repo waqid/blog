a8a44f485361ee9d4a161d069c8ae032e38dd8a4
Nur Waqid Muhsinin <waqid.id@gmail.com>
2022-08-20 23:04:50 +0700

    Section 05 Integrate the Design Part 1

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,16 +1,15 @@
 <x-layout>
-    @foreach ($posts as $post)
-    <article class="{{ $loop->even ? 'bg-blue' : '' }}">
-        <h1>
-            <a href="/posts/{{ $post->slug }}">
-                {{ $post->title }}
-            </a>
-        </h1>
-        <p>
-            By <a href="/authors/{{$post->author->username}}">{{$post->author->name}}</a> in
-            <a href="/categories/{{$post->category->slug }}">{{ $post->category->name }} </a>
-        </p>
-        <p>{{ $post->excerpt }}</p>
-    </article>
-    @endforeach
+    @include('_post-header')
+
+    <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
+        @if($posts->count())
+
+        <x-post-grid :posts="$posts" />
+
+        @else
+        <p class="text-center">No post yet. Please check back later.</p>
+        @endif
+
+    </main>
+
 </x-layout>
